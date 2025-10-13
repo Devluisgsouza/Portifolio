e5a135c0e198113c6667ca97af5d65a317cb70d7
Devluisgsouza <devlgsouza@gmail.com>
2025-10-13 15:25:19 -0300

    ajuste scroll projects

--- reveal.js
+++ reveal.js
@@ -28,7 +28,29 @@ see.reveal('.flex',{
     origin: 'left',
 })
 
+see.reveal('.img-port-1',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+})
+
+see.reveal('.img-port-2',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay: 500
+})
+
+see.reveal('.img-port-3',{
+    duration: 2000,
+    distance: '90px',
+    origin: 'left',
+    delay: 1000
+})
+
 see.reveal('.icons',{
     duration: 2000,
     distance: '90px',
+    origin: 'top',
+    delay: 100
 })
